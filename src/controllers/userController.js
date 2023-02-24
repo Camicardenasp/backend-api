6d83abd60fa6d5a57a8d96d590018273c923f18c
Camilo Cárdenas <camicardenasp@gmail.com>
2023-02-24 04:18:32 -0500

    Added status code.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,31 +1,97 @@
-const User = require("../models/users");
+const userService = require("../services/userService");
 
 const getAllUsers = (req, res) => {
-    User.find((err, result) => {
-        if(err) throw new Error(err);
-        res.json(result);
-    });
+    const { lastname } = req.query;
+    try {
+        const allUser = userService.getAllUsers({ lastname });
+        res.send({ status: "OK", data: allUser });
+    } catch (error) {
+        res
+        .status(error?.status || 500)
+        .send({ status: "FAILED", data: { error: error?.message || error } });
+    }
 };
 
 const createNewUser = (req, res) => {
-    User.create(req.body, (err, result) => {
-        if(err) throw new Error(err);
-        res.json(result);
-    });
+    const { body } = req;
+
+    if (
+        !body.name ||
+        !body.lastname ||
+        !body.email ||
+        !body.password ||
+        !body.is_admin
+    ) {
+        res.status(400).send({
+        status: "FAILED",
+        data: {
+            error:
+            "One of the following keys is missing or is empty in request body: 'name', 'lastname', 'email', 'password', 'is_admin'",
+        },
+        });
+    }
+
+    const newUser = {
+        name: body.name,
+        lastname: body.lastname,
+        email: body.email,
+        password: body.password,
+        is_admin: body.is_admin,
+    };
+
+    try {
+        const createdUser = userService.createNewUser(newUser);
+        res.status(201).send({ status: "OK", data: createdUser });
+    } catch (error) {
+        res
+        .status(error?.status || 500)
+        .send({ status: "FAILDED", data: { error: error?.message || error } });
+    }
 };
 
 const updateOneUser = (req, res) => {
-    User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }, (err, result) => {
-        if(err) throw new Error(err);
-        res.json(result);
-    });
+    const {
+        body,
+        params: { userId },
+    } = req;
+
+    if (!userId) {
+        res.status(400).send({
+        status: "FAILED",
+        data: { error: "Parameter ':userId' can not be empty" },
+        });
+    }
+
+    try {
+        const updatedUser = userService.updateOneUser(userId, body);
+        res.send({ status: "OK", data: updatedUser });
+    } catch (error) {
+        res
+        .status(error?.status || 500)
+        .send({ status: "FAILED", data: { error: error?.message || error } });
+    }
 };
 
 const deleteOneUser = (req, res) => {
-    User.findOneAndRemove({ _id: req.params.id }, (err, result) => {
-        if(err) throw new Error(err);
-        res.end();
-    });
+    const {
+        params: { userId },
+    } = req;
+
+    if (!userId) {
+        res.status(400).send({
+        status: "FAILED",
+        data: { error: "Parameter ':userId' can not be empty" },
+        });
+    }
+
+    try {
+        userService.deleteOneUser(userId);
+        res.status(204).send({ status: "OK" });
+    } catch (error) {
+        res
+        .status(error?.status || 500)
+        .send({ status: "FAILED", data: { error: error?.message || error } });
+    }
 };
 
 module.exports = {
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,16 +1,62 @@
-const getAllUsers = () => {
-    
-    return;
+const { v4: uuid } = require("uuid");
+const User = require("../models/users");
+
+const getAllUsers = (filterParams) => {
+    try {
+        return User.find((err, result) => {
+            if(err) throw new Error(err);
+            res.json(result);
+        });
+    } catch (error) {
+        throw error;
+    }
 };
-const createNewUser = () => {
-    
-    return;
+
+const createNewUser = (newUser) => {
+    const userToInsert = {
+        ...newUser,
+        id: uuid(),
+        createdAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
+        updatedAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
+    };
+    try {
+        const createdUser = User.create(req.body, (err, result) => {
+            if(err) throw new Error(err);
+            res.json(result);
+        });
+        return createdUser;
+    } catch (error) {
+        throw error;
+    }
 };
-const updateOneUser = () => {
-    
-    return;
+
+const updateOneUser = (Id, changes) => {
+    try {
+        const updatedUser = User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }, (err, result) => {
+            if(err) throw new Error(err);
+            res.json(result);
+        });
+        return updatedUser;
+    } catch (error) {
+        throw error;
+    }
 };
-const deleteOneUser = () => {
-    
-    return;
+
+const deleteOneUser = (Id) => {
+    try {
+        User.findOneAndRemove({ _id: req.params.id }, (err, result) => {
+            if(err) throw new Error(err);
+            res.end();
+    });
+    } 
+        catch (error) {
+        throw error;
+    }
+};
+
+module.exports = {
+    getAllUsers,
+    createNewUser,
+    updateOneUser,
+    deleteOneUser,
 };
